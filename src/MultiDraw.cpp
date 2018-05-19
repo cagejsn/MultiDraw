dea8d5ede6dfb73f77a99bdd0388aad4d1482061
Cage <cagejsn@gmail.com>
2018-05-19 10:44:00 -0500

    develop branch

---
+++ src/MultiDraw.cpp
@@ -0,0 +1,13 @@
+//
+//  MultiDraw.cpp
+//  MultiDraw
+//
+//  Created by Cage Johnson on 5/19/18.
+//  Copyright © 2018 Cage Johnson. All rights reserved.
+//
+
+#include "MultiDraw.hpp"
+
+
+
+
---
+++ src/MultiDraw.hpp
@@ -0,0 +1,14 @@
+//
+//  MultiDraw.hpp
+//  MultiDraw
+//
+//  Created by Cage Johnson on 5/19/18.
+//  Copyright © 2018 Cage Johnson. All rights reserved.
+//
+
+#ifndef MultiDraw_hpp
+#define MultiDraw_hpp
+
+#include <stdio.h>
+
+#endif /* MultiDraw_hpp */
